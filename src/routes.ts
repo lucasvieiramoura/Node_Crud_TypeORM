88f06234f8f78e98a6aeb741b23baf73e341b9e1
lucasvieiramoura <lucasvieiramoura@gmail.com>
2022-02-17 19:46:04 -0300

    GetAll, DeleteCategory Routa Controller & Service

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
+import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
+import { GetAllCategoriesController } from "./controllers/GetAllCategoryController";
 
 
 const routes = Router();
 
 routes.post("/categories", new CreateCategoryController().handle);
+routes.get("/categories", new GetAllCategoriesController().handle);
+routes.delete("/categories/:id", new DeleteCategoryController().handle);
 
 export {routes};
