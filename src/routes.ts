ea79fc8285c0fff8e3893e918a924c34f336997e
Lucas Vietos <lucasvieiramoura@gmail.com>
2022-02-19 16:13:55 -0300

    Update Service Controller Routa

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
 import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
 import { GetAllCategoriesController } from "./controllers/GetAllCategoryController";
+import { UpdateCategoryController } from "./controllers/UpdateCategoryController";
 
 
 const routes = Router();
@@ -9,5 +10,6 @@ const routes = Router();
 routes.post("/categories", new CreateCategoryController().handle);
 routes.get("/categories", new GetAllCategoriesController().handle);
 routes.delete("/categories/:id", new DeleteCategoryController().handle);
+routes.put("/categories/:id", new UpdateCategoryController().handle);
 
 export {routes};
---
+++ src/services/CreateVideoService.ts
@@ -0,0 +1,28 @@
+import { getRepository } from "typeorm";
+import { Category } from "../entities/Category";
+import { Video } from "../entities/Videos";
+
+type VideoRequest = {
+    name: string,
+    description: string,
+    duration: string,
+    category_id : string,
+}
+
+export class CreateVideosService {
+    async execute({name, description,duration, category_id}:VideoRequest) : Promise<Error | Video>{
+        const repo = getRepository(Video);
+        const repoCategory = getRepository(Category);
+
+        if(!(await repoCategory.findOne(category_id))){
+            return new Error ("Categoria não existe!");
+        }
+
+        const video = repo.create({name, description,duration, category_id});
+
+        await repo.save(video);
+
+        return video;
+
+    }
+}
