67a5fdf0c9945d6dce8f5082170b949fb9ce7489
Lucas Vietos <lucasvieiramoura@gmail.com>
2022-02-20 20:11:37 -0300

    Create video Get All videos with relation

---
+++ src/services/GetlAllVideosService.ts
@@ -0,0 +1,15 @@
+import { getRepository } from "typeorm";
+import { Video } from "../entities/Videos";
+
+
+export class GetAllVideosService {
+    async execute(){
+        const repo = getRepository(Video);
+
+        const videos = await repo.find({
+            relations: ['category']
+        });
+
+        return videos;
+    }
+}
